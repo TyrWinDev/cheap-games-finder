421da251bdca5a7dce0073a6a5525a7a185aa7f4
Pedro Medina <tyrwinterhd@gmail.com>
2023-10-14 16:45:07 -0400

    Styling of game list search containers for  mobile resolution

--- client/src/components/GameList.jsx
+++ client/src/components/GameList.jsx
@@ -87,6 +87,7 @@ const GameList = ({ games, loading }) => {
           {filteredGames.map((game) => {
             const { metacritic } = game;
             const { rating, ratingClass } = getRating(metacritic);
+            const cheapSharkDealURL = `https://www.cheapshark.com/redirect?dealID=${game.cheapestDealID}`;
 
             return (
               <div className="game-list__game-container" key={game.id}>
@@ -105,16 +106,21 @@ const GameList = ({ games, loading }) => {
                       <h3 id="game-list__title">{game.name}</h3>
                     </Link>
 
-                    <div className="game-list__platforms">
-                      {game.platforms.map((platform) => (
-                        <span
-                          className="game-list__platform"
-                          key={platform.platform.id}
-                        >
-                          <i>{platform.platform.name}</i>
-                        </span>
-                      ))}
+                    <div className={`game-list__rating-container`}>
+                      <span className={`game-list__rating ${ratingClass}`}>
+                        {!game.metacritic ? "N/A" : game.metacritic}
+                      </span>
                     </div>
+                  </div>
+                  <div className="game-list__platforms">
+                    {game.platforms.map((platform) => (
+                      <span
+                        className="game-list__platform"
+                        key={platform.platform.id}
+                      >
+                        <i>{platform.platform.name}</i>
+                      </span>
+                    ))}
                   </div>
                   <div className="game-list__genres">
                     {game.genres.map((genre) => (
@@ -123,41 +129,46 @@ const GameList = ({ games, loading }) => {
                       </span>
                     ))}
                   </div>
-                  <div className="game-list__playtime-rating">
-                    <span id="game-list__released">Release date: {game.released}</span>
+                  {/* <div className="game-list__playtime-rating">
+                    <span id="game-list__released">
+                      Release date: {game.released}
+                    </span>
                     {game.playtime !== 0 && (
                       <h4 id="game-list__playtime">
-                       <AiOutlineClockCircle /> {`Average Playtime: ${game.playtime} hours`}
+                        <AiOutlineClockCircle />{" "}
+                        {`Average Playtime: ${game.playtime} hours`}
                       </h4>
                     )}
-                  </div>
+                  </div> */}
                   <div className="game-list__description">
                     <p>
                       {game.description.length > 120
                         ? game.description.slice(0, 120) + "..."
                         : game.description}
-                         <Link to={`/game/${game.id}`}>... learn More</Link>
+                      {/* <Link to={`/game/${game.id}`}>... learn More</Link> */}
                     </p>
-                   
                   </div>
                 </div>
 
                 <div className="game-list__actions-container">
-                  <div className="game-list__price-container">
-                    <span id="game-list__price">{`Cheapest Price: ${
-                      !game.cheapest ? "N/A" : `$${game.cheapest}`
-                    }`}</span>
-                  </div>
-                  <div className={`game-list__rating-container`}>
-                    <span className={`game-list__rating ${ratingClass}`}>
-                      {!game.metacritic ? "N/A" : game.metacritic}
-                    </span>
-                  </div>
                   <div className="game-list__button-container">
+                    <button className="game-list__button" disabled={!game.cheapest}>
+                      <Link
+                        to={`/game/${game.id}`}
+                        target="_blank"
+                      >
+                        <h3>{!game.cheapest ? "No offers available..." : `Get this game for $${game.cheapest}`}</h3>
+                        {/* <span id="game-list__price">{`Cheapest Price: ${
+                          !game.cheapest ? "N/A" : `$${game.cheapest}`
+                        }`}</span> */}
+                      </Link>
+                    </button>
+                  </div>
+                  {/* <div className="game-list__button-container">
                     <button className="game-list__button">
                     <AiFillStar /> Add to Wishlist
                     </button>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             );
